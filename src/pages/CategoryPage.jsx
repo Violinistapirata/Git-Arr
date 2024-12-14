1f66a7ac3aa1fef78a389eeb408f828cc71692c3
Sven Andersson <sven@andersson.es>
2024-12-14 14:00:20 +0100

    category page ready

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,6 +1,7 @@
 //HOOKS
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 //SUPABASE
 import supabase from "../supabase/config";
@@ -14,71 +15,114 @@ import "./CategoryPage.css";
 /* ------------------------------------------------------- */
 
 function CategoryPage() {
-	const [category,setCategory] = useState(0)
+	const [category, setCategory] = useState(null);
 	const [products, setProducts] = useState([]);
-    const [order,setOrder] = useState({orderBy: "title", ascending: true})
+	const [catOrder, setCatOrder] = useState({
+		orderBy: "title",
+		ascending: true,
+	});
+
+	const navigate = useNavigate();
 
 	const { categoryId } = useParams();
 
-
 	const getProducts = async () => {
 		try {
-			const response = await supabase
+			const { data } = await supabase
 				.from("products")
 				.select("*")
 				.eq("category", `${categoryId}`)
-                .order(order.orderBy, { ascending: order.ascending });
-                setProducts(response.data);
-			// console.log("this is the products response", response);
+				.order(catOrder.orderBy, { ascending: catOrder.ascending });
+			//console.log("this is data: ", data);
+
+			setProducts(data);
 		} catch (error) {
 			console.error(error);
 		}
 	};
 	const getCategory = async () => {
 		try {
-			const response = await supabase
+			const { data } = await supabase
 				.from("products_categories")
 				.select("*")
 				.eq("id", `${categoryId}`);
-                setCategory(response.data[0]);
-			// console.log("this is the category response", response);
+
+			//console.log(data[0]);
+			if (data) {
+				setCategory(data[0]);
+			} else {
+				navigate("/404");
+			}
 		} catch (error) {
 			console.error(error);
 		}
 	};
 
-    useEffect(() => {
-        getProducts();
-        getCategory();
-    },[])
+	const saveOrderBy = (orderby) => {
+		setCatOrder({ ...catOrder, orderBy: orderby });
+	};
+	const saveOrder = (order) => {
+		setCatOrder({ ...catOrder, ascending: order });
+	};
 
-    console.log("state products", products);
-    console.log("state category", category);
-    
-    
-	return (
-		<>
-			<h1>{category.category_name}</h1>
-            <div className="order-bar">
-                <div className="order-by">
-                    <label htmlFor="orderBy">Order by</label>
-                    <select>
-                        <option value=""></option>
-                    </select>
-                </div>
-                <div className="order">
-                    <label htmlFor="order">Order</label>
-                    <input type="radio" name="order" id="" value={true} checked="checked"/>
-                    <input type="radio" name="order" id="" value={false}/>
-                </div>
-            </div>
-			<section className="product-list">
-				{products.map((product) => {
-					return <ProductCard key={product.id} product={product} />;
-				})}
-			</section>
-		</>
-	);
+	useEffect(() => {
+		getCategory();
+	}, []);
+
+	useEffect(() => {
+		getProducts();
+	}, [catOrder]);
+
+	// console.log("state products", products);
+	// console.log("state category", category);
+
+	if (category !== null) {
+		return (
+			<>
+				<h1>{category.category_name}</h1>
+				<div className="order-bar">
+					<div className="order-by">
+						<label htmlFor="orderBy">Order by</label>
+						<select
+							onChange={(e) => {
+								saveOrderBy(e.target.value);
+							}}
+							defaultValue={catOrder.orderBy}
+						>
+							<option value="title">title</option>
+							<option value="price">price</option>
+						</select>
+					</div>
+					<div className="order">
+						<label htmlFor="asc">Order ascending</label>
+						<input
+							onClick={() => {
+								saveOrder(true);
+							}}
+							type="radio"
+							name="order"
+							id="asc"
+							defaultChecked={true}
+						/>
+						<label htmlFor="desc">Order descending</label>
+						<input
+							onClick={() => {
+								saveOrder(false);
+							}}
+							type="radio"
+							name="order"
+							id="desc"
+						/>
+					</div>
+				</div>
+				<section className="product-list">
+					{products.map((product) => {
+						return <ProductCard key={product.id} product={product} />;
+					})}
+				</section>
+			</>
+		);
+	}
 }
 
 export default CategoryPage;
