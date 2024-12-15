ccd0a47fcc0abc0ff2322287096f91de8d7312e5
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-12-15 19:34:50 +0100

    refactoring of Featured Products done

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 
 // COMPONENTS
 import ProductCard from "./ProductCard";
+
+// STYLES
 import './ProductsList.css'
 /* ----------------------------------------------- */
 
@@ -15,30 +17,16 @@ function FeaturedProducts() {
   useEffect(() => {
     async function getFeaturedProducts() {
       try {
-        const { data, error } = await supabase
+        const { data } = await supabase
           .from("products")
           .select()
           .eq("featured", true);
-        console.log("THIS IS THE ARRAY OF FEATURED PRODUCTS", data);
         setFeatured(data);
 
       } catch (error) {
         console.error(error);
       }
     }
-
-    /*  async function getFeaturedProducts() {
-      try {
-        await featureProducts();
-        const response = await supabase
-          .from("products_fetured")
-          .select("products (image, title, category, price, id)");
-       
-        console.log(featured);
-      } catch (error) {
-        console.error(error);
-      }
-    } */
     getFeaturedProducts();
   }, []);
 
