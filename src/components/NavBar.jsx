4157d786b0e78981bedbdd6dae36e419cefe3501
Sven Andersson <sven@andersson.es>
2024-12-16 20:43:24 +0100

    move search bar to separate component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,76 +1,67 @@
 import logo from "../assets/logo.png";
 import cart from "../assets/cart-logo.svg";
-import glass from "../assets/glass.svg";
 import "./NavBar.css";
-import {Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 // import CartPage from "../pages/CartPage";
+import SearchBar from "./SearchBar";
 import { useEffect, useState } from "react";
 import supabase from "../supabase/config";
 
 function NavBar() {
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    async function getCategories() {
-      try {
-        const { data, error } = await supabase
-          .from("products_categories")
-          .select("id, category_name, category_image");
-        setCategories(data);
-        if (error) {
-          throw error;
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getCategories();
-  }, []);
+	const [categories, setCategories] = useState([]);
+	useEffect(() => {
+		async function getCategories() {
+			try {
+				const { data, error } = await supabase
+					.from("products_categories")
+					.select("id, category_name, category_image");
+				setCategories(data);
+				if (error) {
+					throw error;
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		getCategories();
+	}, []);
 
-  return (
-    <>
-      <nav className="nav">
-        <div className="nav-left">
-          <Link to="/">
-          <img className="logo" src={logo} />
-          </Link>
-          <Link to="/">
-          {" "}
-          <h1 className="brand-name">Git-Arr</h1>{" "}
-          </Link>
-        </div>
-        <form className="search-bar">
-          <input
-            className="search-container"
-            type="text"
-            placeholder="search guitars"
-          />
-          <button className="search-button" type="button">
-            <img className="glass" src={glass} alt="Search" />
-          </button>
-        </form>
-        <div className="nav-right">
-          <Link to="/cart">
-          <img className="cart" src={cart} alt="Cart" />
-          </Link>
-        </div>
-      </nav>
-      <div className="categories">
-        <ul className="category-list">
-        {categories ? (
-          categories.map((category) => {
-            return (
-              <Link to={`/category/${category.id}`} key={category.id}>
-              <li className="list">{category.category_name}</li>
-              </Link>
-            );
-          })
-        ) : (
-          <p>No categories available</p>
-        )}
-        </ul>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<nav className="nav">
+				<div className="nav-left">
+					<Link to="/">
+						<img className="logo" src={logo} />
+					</Link>
+					<Link to="/">
+						{" "}
+						<h1 className="brand-name">Git-Arr</h1>{" "}
+					</Link>
+				</div>
+				<SearchBar />
+				<div className="nav-right">
+					<Link to="/cart">
+						<img className="cart" src={cart} alt="Cart" />
+					</Link>
+				</div>
+			</nav>
+			<div className="categories">
+				<ul className="category-list">
+					{categories ? (
+						categories.map((category) => {
+							return (
+								<Link to={`/category/${category.id}`} key={category.id}>
+									<li className="list">{category.category_name}</li>
+								</Link>
+							);
+						})
+					) : (
+						<p>No categories available</p>
+					)}
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default NavBar;
