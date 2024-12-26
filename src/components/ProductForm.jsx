d41934e73fd5fb1dc4740b897be92836270b7578
Sven Andersson <sven.andersson@lantmannen.com>
2024-12-26 12:39:57 +0100

    update product works

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -5,132 +5,163 @@ import { useEffect, useState } from "react";
 import supabase from "../supabase/config";
 
 function ProductForm({ productForm, setProductForm }) {
-	console.log("productForm show", productForm.show);
-	console.log("productForm id", productForm.id);
-    
-    
-	const productColumns = {
-		title: "",
-		image: "",
-		description: "",
-		price: undefined,
-		stock: 0,
-		featured: false,
-	};
+  console.log("productForm show:", productForm.show);
+  console.log("productForm id:", productForm.id);
 
-	const [form, setForm] = useState(productColumns);
-    console.log(form);
+  const productColumns = {
+    title: "",
+    image: "",
+    description: "",
+    price: 0,
+    stock: 0,
+    featured: false,
+  };
 
-	const getProduct = async (id) => {
-		try {
-			const { data, error } = await supabase
-				.from("products")
-				.select("*, products_categories(category_name)")
-				.eq("id", `${id}`);
+  const [form, setForm] = useState(productColumns);
+  console.log("setForm form:", form);
 
-			setProductForm({
-				title: data.title,
-				image: data.image,
-				description: data.description,
-				price: data.price,
-				stock: data.stock,
-				featured: data.featured,
-			});
+  const getProduct = async (id) => {
+    try {
+      const { data, error } = await supabase
+        .from("products")
+        .select("*, products_categories(category_name)")
+        .eq("id", `${id}`);
 
-			if (error) {
-				throw error;
-			}
-		} catch (error) {
-			console.error(error);
-		}
-        console.log(setProductForm);
-        
-	};
+      console.log("product from db", data);
 
-	const handleInput = (event) => {
-		const value = event.target.value;
-		setForm({
-			...form,
-			[event.target.name]: value,
-		});
-	};
+      setForm({
+        id: data[0].id,
+        title: data[0].title,
+        image: data[0].image,
+        description: data[0].description,
+        price: data[0].price,
+        stock: data[0].stock,
+        featured: data[0].featured,
+      });
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const editProduct = {
-			title: form.title,
-			image: form.image,
-			description: form.description,
-			price: form.price,
-			stock: form.stock,
-			featured: form.featured,
-		};
-		//props.handleAddStudent(newStudent);
-	};
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	useEffect(() => {
-		if (productForm === "edit") {
-			getProduct(productForm.id);
-		}
-	}, [productForm]);
+  const updateProduct = async (id, object) => {
+    try {
+      console.log(object);
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<span>Edit</span>
-			<div>
-				<label>
-					Title
-					<input
-						value={form.title}
-						onChange={handleInput}
-						name="title"
-						type="text"
-						placeholder="Product name"
-					/>
-				</label>
-				<label>
-					Image
-					<input
-						value={form.image}
-						onChange={handleInput}
-						name="image"
-						type="text"
-						placeholder="Product image url"
-					/>
-				</label>
-				<label>
-					Description
-					<textarea
-						value={form.description}
-						onChange={handleInput}
-						name="description"
-						type="text"
-						placeholder="Product description"
-						rows="5"
-					/>
-				</label>
-				<label>
-					Price €
-					<input
-						value={form.price}
-						onChange={handleInput}
-						name="price"
-						type="number"
-						placeholder="Product price"
-					/>
-				</label>
-				<label>
-					Stock
-					<input
-						value={form.stock}
-						onChange={handleInput}
-						name="stock"
-						type="number"
-						placeholder="Products in stock"
-					/>
-				</label>
+      const { data, error } = await supabase
+        .from("products")
+        .update(object)
+        .eq("id", id)
+        .select();
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-				{/* 
+  const handleInput = (event) => {
+    const value = event.target.value;
+    setForm({
+      ...form,
+      [event.target.name]: value,
+    });
+  };
+
+  /* 
+const { data, error } = await supabase
+  .from('products')
+  .update({ other_column: 'otherValue' })
+  .eq('some_column', 'someValue')
+  .select()
+*/
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const editProduct = {
+      title: form.title,
+      image: form.image,
+      description: form.description,
+      price: form.price,
+      stock: form.stock,
+      featured: form.featured,
+    };
+    console.log("editProduct object", editProduct);
+    if (productForm.show === "edit") {
+      updateProduct(form.id, editProduct);
+    }
+    //props.handleAddStudent(newStudent);
+  };
+
+  useEffect(() => {
+    if (productForm.show === "edit") {
+      getProduct(productForm.id);
+      console.log("useEffect === edit");
+    }
+  }, [productForm.id]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <span>Edit</span>
+      {form.id && <input value={form.id} name="id" type="hidden" />}
+      <div>
+        <label>
+          Title
+          <input
+            value={form.title}
+            onChange={handleInput}
+            name="title"
+            type="text"
+            placeholder="Product name"
+          />
+        </label>
+        <label>
+          Image
+          <input
+            value={form.image}
+            onChange={handleInput}
+            name="image"
+            type="text"
+            placeholder="Product image url"
+          />
+        </label>
+        <label>
+          Description
+          <textarea
+            value={form.description}
+            onChange={handleInput}
+            name="description"
+            type="text"
+            placeholder="Product description"
+            rows="5"
+          />
+        </label>
+        <label>
+          Price €
+          <input
+            value={form.price}
+            onChange={handleInput}
+            name="price"
+            type="number"
+            placeholder="Product price"
+          />
+        </label>
+        <label>
+          Stock
+          <input
+            value={form.stock}
+            onChange={handleInput}
+            name="stock"
+            type="number"
+            placeholder="Products in stock"
+          />
+        </label>
+
+        {/* 
                 
 
 			price: form.price,
@@ -139,9 +170,9 @@ function ProductForm({ productForm, setProductForm }) {
 
                  */}
 
-				<button type="submit">Save</button>
-			</div>
-		</form>
-	);
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  );
 }
 export default ProductForm;
