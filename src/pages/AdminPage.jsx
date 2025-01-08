2ced326f66f3f1a74c926fcf5f8df7638c181d13
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-01-08 19:39:18 +0100

    move add product button to product list section and duplicate it in the categories section

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,32 +1,57 @@
 //COMPONENTS
-import ProductsList from "../components/ProductsList"
-import Categories from "../components/Categories"
-import ProductForm from "../components/ProductForm"
-import CategoryForm from "../components/CategoryForm"
-import { useState } from "react"
+import ProductsList from "../components/ProductsList";
+import Categories from "../components/Categories";
+import ProductForm from "../components/ProductForm";
+import CategoryForm from "../components/CategoryForm";
 
+//HOOKS
+import { useState } from "react";
 
-function AdminPage({categories, getCategories}) {
-  const [productForm, setProductForm] = useState({show:"none", id:""})
-  const [categoryForm, setCategoryForm] = useState({show:"none", id:""})
-  
+/* ----------------------------------------------- */
+
+function AdminPage({ categories, getCategories }) {
+  const [productForm, setProductForm] = useState({ show: "none", id: "" });
+  const [categoryForm, setCategoryForm] = useState({ show: "none", id: "" });
+
   return (
     <div>
-        <h1>ADMIN PAGE</h1>
-        <section>
-            <ProductsList setProductForm={setProductForm} productForm={productForm}/>
-        </section>
-        <section>
-            <Categories categories={categories} getCategories={getCategories} setCategoryForm={setCategoryForm}/>
-
-        </section>
-        <section>
-          {(productForm.show === "add" || productForm.show === "edit") && <ProductForm productForm={productForm} setProductForm={setProductForm} /> }
-          {(categoryForm.show === "add" || categoryForm.show === "edit") && <CategoryForm getCategories={getCategories} categoryForm={categoryForm} setCategoryForm={setCategoryForm} /> }
-        </section>
-        <button onClick={() => setProductForm({show:"add",id:""})} >Add product</button>
+      <h1>ADMIN PAGE</h1>
+      <section>
+        <ProductsList
+          setProductForm={setProductForm}
+          productForm={productForm}
+        />
+        <button onClick={() => setProductForm({ show: "add", id: "" })}>
+          Add product
+        </button>
+      </section>
+      <section>
+        <Categories
+          categories={categories}
+          getCategories={getCategories}
+          setCategoryForm={setCategoryForm}
+        />
+        <button onClick={() => setProductForm({ show: "add", id: "" })}>
+          Add category
+        </button>
+      </section>
+      <section>
+        {(productForm.show === "add" || productForm.show === "edit") && (
+          <ProductForm
+            productForm={productForm}
+            setProductForm={setProductForm}
+          />
+        )}
+        {(categoryForm.show === "add" || categoryForm.show === "edit") && (
+          <CategoryForm
+            getCategories={getCategories}
+            categoryForm={categoryForm}
+            setCategoryForm={setCategoryForm}
+          />
+        )}
+      </section>
     </div>
-  )
+  );
 }
 
-export default AdminPage
+export default AdminPage;
