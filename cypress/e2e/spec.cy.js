80b1895feba6654561fe7c408606bc3694bd60f3
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-01-16 20:02:31 +0100

    Fixed tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,10 @@
 describe('template spec', () => {
   it('has a home page with nav bar, featured products, categories and a footer', () => {
-    // cy.intercept('GET', 'https://whpbdrinqtvellfpwtgs.supabase.co').as('getItems');
     cy.visit('http://localhost:1212');
     cy.get('.nav').contains('Git-Arr');
-    // cy.wait('@getItems');
-    // cy.get('h1').contains('Featured products').parent().contains('.product-list');
-    cy.get('section').contains('.categories-wrapper');
+    cy.get('h1').contains('Featured products');
+    cy.get('article').first().should('have.class', 'product-card');
+    cy.get('.product-card').first();
     cy.get('.footer').contains('About us');
   })
 })
