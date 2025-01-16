59a19e4cb4cf681a8e58e291ad5f045f133d808d
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-01-16 20:32:17 +0100

    added tests for the product details view

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,17 @@ describe('template spec', () => {
     cy.get('.nav').contains('Git-Arr');
     cy.get('h1').contains('Featured products');
     cy.get('article').first().should('have.class', 'product-card');
-    cy.get('.product-card').first();
     cy.get('.footer').contains('About us');
   })
+  it('has a view for the products details', () => {
+    cy.visit('http://localhost:1212');
+    cy.get('.product-card').first().click();
+    cy.get('h1').should('have.class', 'product-details-title').contains('Seagull S6 Original');
+    cy.get('p').should('have.class', 'product-details-description').contains('Canadian-made acoustic guitar known for its full, rich sound');
+    cy.get('h4').first().should('have.class', 'product-details-categories').contains('Flamenco guitars');
+    cy.get('h4').last().should('have.class', 'product-details-price').contains('€');
+    cy.get('h2').should('have.class', 'product-details-stock').contains('IN STOCK: 12');
+    cy.get('button').should('have.class', 'add-to-cart-button').contains('Add to cart');
+  })
+
 })
