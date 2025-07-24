66ede9fff2bc3313042fe4f1b98e1f1156737724
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-07-24 18:35:03 +0200

    restarted git-arr database

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
-);
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 export default supabase;
